c1608e719cfc4cb8572e4faefac30c423144e055
André Comegno <55299009+andrecomegno@users.noreply.github.com>
2024-01-24 19:53:53 -0300

    criado formulario
    - foi criado formulário

--- src/resgatpet/usuarios.module.ts
+++ src/resgatpet/usuarios.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
+import { FormularioController as  FormularioController } from "./formulario/formulario.controller";
+import { FormularioArmazenados as FormularioArmazenados } from "./formulario/formulario.dm";
 import { UsuarioController as UsuarioController } from "./usuario/usuario.controller"; 
 import { UsuarioArmazenados as UsuarioArmazenados } from "./usuario/usuario.dm";
 
 @Module({
-    controllers:[UsuarioController],
-    providers: [UsuarioArmazenados]
+    controllers:[UsuarioController, FormularioController],
+    providers: [UsuarioArmazenados, FormularioArmazenados]
 })
 
 export class UsuarioModule{}
