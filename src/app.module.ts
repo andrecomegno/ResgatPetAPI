6bec72d8263e13bbb30380c2aea3e891b65d8bb3
André Comegno <55299009+andrecomegno@users.noreply.github.com>
2024-03-18 22:25:56 -0300

    removido database e genero
    removido database e genero

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { ArquivoModule } from './resgatpet/arquivo.module';
 import { FormularioModule } from './resgatpet/formulario.module';
 import { UsuarioModule } from './resgatpet/usuarios.module';
-import { GeneroModule } from './resgatpet/genero/genero.module';
 
 @Module({
-  imports: [UsuarioModule, GeneroModule, FormularioModule, ArquivoModule],
+  imports: [UsuarioModule, FormularioModule, ArquivoModule],
   controllers: [],
   providers: [],
 })
