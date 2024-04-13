97599d5d2f41d13bc4983a111a4e7c960a7b1739
André Comegno <55299009+andrecomegno@users.noreply.github.com>
2024-04-12 22:28:39 -0300

    Login fix
    Login fix

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -5,12 +5,18 @@ export const databaseProviders = [
     provide: 'DATA_SOURCE',
     useFactory: async () => {
       const dataSource = new DataSource({
+        // type: 'mysql',
+        // host: '50.116.112.16',
+        // port: 3306,
+        // username: 'vitali04_resgatpet',
+        // password: 'resgatpet4',
+        // database: 'vitali04_resgatpet',
         type: 'mysql',
-        host: '50.116.112.16',
+        host: 'localhost',
         port: 3306,
-        username: 'vitali04_resgatpet',
-        password: 'resgatpet4',
-        database: 'vitali04_resgatpet',
+        username: 'root',
+        password: '',
+        database: 'resgatpet',
         entities: [
             __dirname + '/../**/*.entity{.ts,.js}',
         ],
