7cfb2aced146fceed0718b49350b0d514e7e9a60
Murilo de Sousa <150198914+murilo966@users.noreply.github.com>
2024-04-15 21:39:43 -0300

    Resolvendo bug

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -5,22 +5,22 @@ export const databaseProviders = [
     provide: 'DATA_SOURCE',
     useFactory: async () => {
       const dataSource = new DataSource({
-        // type: 'mysql',
-        // host: '50.116.112.16',
-        // port: 3306,
-        // username: 'vitali04_resgatpet',
-        // password: 'resgatpet4',
-        // database: 'vitali04_resgatpet',
         type: 'mysql',
-        host: 'localhost',
+        host: '50.116.112.16',
         port: 3306,
-        username: 'root',
-        password: '',
-        database: 'resgatpet',
+        username: 'vitali04_resgatpet',
+        password: 'resgatpet4',
+        database: 'vitali04_resgatpet',
+        // type: 'mysql',
+        // host: 'localhost',
+        // port: 3306,
+        // username: 'root',
+        // password: '',
+        // database: 'resgatpet',
         entities: [
             __dirname + '/../**/*.entity{.ts,.js}',
         ],
-        synchronize: true,
+        synchronize: false,
         connectTimeout: 15000, // Tempo limite de 15 segundos (em milissegundos)
       });
       
