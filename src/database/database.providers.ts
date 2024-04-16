5867eb4859e279b55387351e930da62cab34e19d
André Comegno <55299009+andrecomegno@users.noreply.github.com>
2024-04-15 21:28:01 -0300

    Update database.providers.ts

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -15,7 +15,7 @@ export const databaseProviders = [
         host: 'localhost',
         port: 3306,
         username: 'root',
-        password: '123456',
+        password: '',
         database: 'resgatpet',
         entities: [
             __dirname + '/../**/*.entity{.ts,.js}',
